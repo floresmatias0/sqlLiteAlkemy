a8a998b09ff5338cbf0940455d9251142c1f818f
floresmatias <floresmatias0@gmail.com>
2021-11-15 15:52:38 -0300

    practice sql lite

---
+++ dump.sqlite.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE Curso (
+  Codigo INTEGER NOT NULL,
+  Nombre VARCHAR NOT NULL,
+  Descripcion VARCHAR,
+  Turno Varchar NOT NULL, Cupo INTEGER,
+  PRIMARY KEY(codigo)
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
